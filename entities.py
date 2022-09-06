0dd7cb028fa68c9d64dd07ed0a9f1914a4b837aa
tromsky <tromsky@gmail.com>
2022-09-06 16:23:54 -0400

    Add entities

---
+++ entities.py
@@ -0,0 +1,45 @@
+from datetime import date
+from decimal import Decimal
+
+from pony.orm import *
+
+db = Database()
+db.bind(provider="sqlite", filename="budget.db", create_db=True)
+
+
+class AccountType(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    accounts = Set("Account")
+
+
+class Account(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    account_type = Required(AccountType)
+    name = Required(str)
+    transaction_details = Set("TransactionDetail")
+    budget = Optional("Budget")
+
+
+class Transaction(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    effective_date = Required(date)
+    note = Optional(str)
+    transaction_details = Set("TransactionDetail")
+
+
+class TransactionDetail(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    account = Required(Account)
+    transaction = Required(Transaction)
+    amount = Required(Decimal)
+    note = Optional(str)
+
+
+class Budget(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    amount = Required(Decimal)
+    account = Required(Account)
+
+
+db.generate_mapping(create_tables=True)
