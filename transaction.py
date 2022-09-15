e6d9f176f7a169d90047db62c9c23a97fad8c8cc
tromsky <100031072+tromsky@users.noreply.github.com>
2022-09-15 08:41:57 -0400

    Delete transaction (#5)
    * Add update method
    
    * Clean up update method
    
    * Updates to Transaction
     - Save now updates if object has key
     - Update now writes object state to database
     - Transaction returns the same object if one with the same header is requested
    
    * Add delete and reverse methods
    
    * Adding comment about cache
    
    * Clear up docstring
    
    * Fix bad merge conflict fixes, bit of clean-up

--- transaction.py
+++ transaction.py
@@ -3,26 +3,41 @@ TODO
 """
 
 from datetime import date
+from shutil import ExecError
 from weakref import WeakValueDictionary
 
-from constants import TRANSACTION_DETAIL_TYPE_CHOICES, TransactionDetailTypes
 from entities import *
 
 
 class Transaction:
     """
-    Class that handles creating, getting, updating and deleting transactions
+    Class that handles creating, getting, updating, and deleteing transactions
     which are a combination of a TransactionHeader and pairs of
     TransactionDetails
     """
 
+    # cached so factories return the same object reference
     _cache = WeakValueDictionary()
+    __tracked_attrs = [
+        "in_account",
+        "out_account",
+        "amount",
+        "note",
+        "effective_date",
+    ]
 
     def __new__(cls, *_, **kwargs):
         """
         Override new
         If an object with the same header id exists return that object
         """
+
+        # TODO: If you
+        #   1. create a new Transaction
+        #   2. save it
+        #   3. get a transaction with the same header id
+        # the objects will not be equal but if you get get two Transactions
+        # with the same header id, they will be...
 
         header_id = kwargs.get("__header_id")
         obj = cls._cache.get(header_id)
@@ -50,12 +65,19 @@ class Transaction:
         self.effective_date = kwargs.get("effective_date", date.today())
         self.amount = amount
         self.note = kwargs.get("note", "")
+        self.saved = kwargs.get("__saved", False)
+        self.deleted = kwargs.get("__deleted", False)
         # private vars
         self.__header_id = kwargs.get("__header_id")
         self.__in_transaction_detail_id = kwargs.get("__in_transaction_detail_id")
         self.__out_transaction_detail_id = kwargs.get("__out_transaction_detail_id")
 
         print(self)
+
+    def __setattr__(self, key, value):
+        if key in self.__tracked_attrs:
+            setattr(self, "saved", False)
+        super().__setattr__(key, value)
 
     def __repr__(self):
         """
@@ -70,6 +92,7 @@ class Transaction:
             ${self.amount} into {self.in_account.name} [{self.__in_transaction_detail_id}]
             from {self.out_account.name} [{self.__out_transaction_detail_id}], 
             header ID {self.__header_id}
+            Saved: {self.saved}
             """
 
         return f"""
@@ -77,7 +100,7 @@ class Transaction:
             Noted {self.note},
             ${self.amount} into {self.in_account.name} 
             from {self.out_account.name}, 
-            unsaved
+            Saved: {self.saved}
             """
 
     def __str__(self):
@@ -87,37 +110,6 @@ class Transaction:
 
         return self.__repr__()
 
-    def _get(self, header_id):
-        """
-        Private method for getting a transaction on an existing
-        object
-
-        This _method is intented to be used after an update call
-        but shares a lot in common with the class method...how to
-        keep it DRY?
-        """
-
-        transaction_header = TransactionHeader.get(id=header_id)
-        transaction_details = transaction_header.transaction_details
-        for transaction_detail in transaction_details:
-            if transaction_detail.amount < 0:
-                out_account = transaction_detail.account
-                out_transaction_detail_id = transaction_detail.id
-            else:
-                in_account = transaction_detail.account
-                in_transaction_detail_id = transaction_detail.id
-                amount = transaction_detail.amount
-
-        self.in_account = in_account
-        self.out_account = out_account
-        self.amount = amount
-        self.note = transaction_header.note
-        self.effective_date = transaction_header.effective_date
-        self.in_transaction_detail_id = in_transaction_detail_id
-        self.out_transaction_detail_id = out_transaction_detail_id
-
-        return self
-
     @classmethod
     def get(cls, header_id):
         """
@@ -125,14 +117,20 @@ class Transaction:
         """
 
         transaction_header = TransactionHeader.get(id=header_id)
+        if not transaction_header:
+            # no results
+            return None
+
         transaction_details = transaction_header.transaction_details
 
         for transaction_detail in transaction_details:
             if transaction_detail.amount < 0:
                 out_account = transaction_detail.account
                 out_transaction_detail_id = transaction_detail.id
+                out_transaction_detail_id = transaction_detail.id
             else:
                 in_account = transaction_detail.account
+                in_transaction_detail_id = transaction_detail.id
                 in_transaction_detail_id = transaction_detail.id
                 amount = transaction_detail.amount
 
@@ -145,13 +143,18 @@ class Transaction:
             __header_id=transaction_header.id,
             __in_transaction_detail_id=in_transaction_detail_id,
             __out_transaction_detail_id=out_transaction_detail_id,
+            __saved=True,
         )
 
     def save(self):
         """
         Commit the transaction, save to the database
         If the object exists in the database, update it
+        If the object exists in the database, update it
         """
+
+        if self.saved:
+            raise ValueError(f"Transaction {self.__header_id} has already been saved")
 
         if self.__header_id:
             self._update()
@@ -178,11 +181,51 @@ class Transaction:
             self.__in_transaction_detail_id = transaction_detail_in.id
             self.__out_transaction_detail_id = transaction_detail_out.id
 
+        self.saved = "True"
+
+    def reverse(self):
+        """
+        Reverses a transaction while keeping its records
+        """
+
+        if not self.__header_id:
+            raise ExecError("Cannot revserse an unsaved transaciton")
+
+        self.in_account, self.out_account = self.out_account, self.in_account
+        self.saved = False
+        self.note = f"Reversing transaction for {self.__header_id}"
+        self.effective_date = date.today()
+        self.__header_id = None
+        self.__in_transaction_detail_id = None
+        self.__out_transaction_detail_id = None
+
+        return self
+
+    def delete(self):
+        """
+        Hard delete transaction
+        """
+
+        if not self.__header_id:
+            raise ExecError("Cannot delete and unsaved transaction")
+
+        transaction_header = TransactionHeader.get(id=self.__header_id)
+        transaction_header.delete()  # cascade deletes the details
+        commit()
+
+        self.deleted = True
+        self.saved = False
+        self.__header_id = None
+        self.__in_transaction_detail_id = None
+        self.__out_transaction_detail_id = None
+
     @property
     def valid(self):
         """
         Ensure that the transaction is valid before committing
         This probably doesn't need to be here
+
+        TODO: Determine if this is actually needed
 
         TODO: Determine if this is actually needed
         """
