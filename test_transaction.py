41203ab875f66f65fe9e9fb9915785f2181e9dbe
tromsky <tromsky@gmail.com>
2022-09-18 15:15:20 -0400

    Update tests, add delete cases

--- test_transaction.py
+++ test_transaction.py
@@ -4,6 +4,7 @@ Unit tests for transaction
 
 import unittest
 from datetime import date
+from shutil import ExecError
 
 from entities import (
     Account,
@@ -13,6 +14,7 @@ from entities import (
     commit,
     db,
     db_session,
+    delete,
 )
 from transaction import Transaction
 
@@ -21,17 +23,26 @@ db.generate_mapping(create_tables=True)
 
 
 class TestTransaction(unittest.TestCase):
+    @db_session
     def setUp(self):
-        pass
+        delete(account_type for account_type in AccountType)
+        self.balance = AccountType(name="balance")
+        self.expense = AccountType(name="expense")
+        self.chequing = Account(name="chequing", account_type=self.balance)
+        self.rent = Account(name="rent", account_type=self.expense)
+        commit()
+
+        self.transaction = Transaction(
+            self.rent, self.chequing, 100, note="Test delete"
+        )
+        self.transaction.save()
+        self.test_header_id = self.transaction._Transaction__header_id
 
     @db_session
     def test_create(self):
 
-        balance = AccountType(name="balance")
-        expense = AccountType(name="expense")
-        chequing = Account(name="chequing", account_type=balance)
-        rent = Account(name="rent", account_type=expense)
-        commit()
+        rent = Account.get(name="rent")
+        chequing = Account.get(name="chequing")
 
         transaction = Transaction(rent, chequing, 100, note="Hello")
         self.assertFalse(transaction.saved)
@@ -53,24 +64,46 @@ class TestTransaction(unittest.TestCase):
     @db_session
     def test_get(self):
 
-        balance = AccountType(name="balance")
-        expense = AccountType(name="expense")
-        chequing = Account(name="chequing", account_type=balance)
-        rent = Account(name="rent", account_type=expense)
-        commit()
+        rent = Account.get(name="rent")
+        chequing = Account.get(name="chequing")
 
         transaction = Transaction(rent, chequing, 100, note="Hello")
         transaction.save()
         header_id = transaction.header_id
-
+        in_account = transaction.in_account
+        out_account = transaction.out_account
+        del transaction
         existing_transaction = Transaction.get(header_id)
 
         self.assertTrue(existing_transaction.saved)
         self.assertEqual(existing_transaction.note, "Hello")
         self.assertEqual(existing_transaction.effective_date, date.today())
         self.assertEqual(existing_transaction.amount, 100)
-        self.assertEqual(transaction.in_account, rent)
-        self.assertEqual(transaction.out_account, chequing)
+        self.assertEqual(in_account, rent)
+        self.assertEqual(out_account, chequing)
+
+    @db_session
+    def test_delete(self):
+
+        transaction = Transaction.get(self.test_header_id)
+
+        transaction.delete()
+        deleted_transaction = Transaction.get(self.test_header_id)
+
+        self.assertIsNone(deleted_transaction)
+        self.assertTrue(transaction.deleted)
+        self.assertFalse(transaction.saved)
+        self.assertIsNone(transaction.header_id)
+
+    @db_session
+    def test_delete_new_transaction(self):
+
+        rent = Account.get(name="rent")
+        chequing = Account.get(name="chequing")
+
+        transaction = Transaction(rent, chequing, 100, note="Hello")
+
+        self.assertRaises(ExecError, transaction.delete)
 
 
 if __name__ == "__main__":
